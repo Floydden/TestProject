2256d54feede41df0e4aab646ea2ca5fb17d1fef
Floydden <dennis.a.melnikov@gmail.com>
2019-11-28 23:42:38 +0400

    Test message for the first commit to repo.

--- src/com/example/testproject/TestProject.java
+++ src/com/example/testproject/TestProject.java
@@ -1,7 +1,35 @@
 package com.example.testproject;
 
-public class TestProject {
-    public static void main(String[] args) {
-        System.out.println("What the fuck is going on?");
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+class Dog {
+    int age; // возраст
+    String name; // кличка
+
+    public void voice() {
+        for (int i = 1; i <= age; i++) {
+            System.out.println("гав-гав");
+        }
+    }
+    public Dog(String n, int a) {
+        name = n;
+        age = a;
+    }
+    public Dog(){
+        name = "unknown";
     }
 }
+
+
+public class TestProject {
+    public static void main(String[] args) {
+       Dog Sobaka = new Dog("Tratata", 3);
+       Sobaka.voice();
+       System.out.println(Sobaka.name + " " + Sobaka.age);
+
+       Dog tt = new Dog();
+       System.out.println(tt.name);
+    }
+}
+
